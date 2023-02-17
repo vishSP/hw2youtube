d282ce90161094d216ce54dfbd9135a8dc691b02
vishSP <hihi64741@gmail.com>
2023-02-17 22:49:25 +0200

    написал код, тест, подправил гитигнор

--- utils.py
+++ utils.py
@@ -2,12 +2,14 @@ import os
 from googleapiclient.discovery import build
 import json
 from dotenv import load_dotenv, find_dotenv
+
 """скрываю АПИ"""
 load_dotenv(find_dotenv())
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
 
-"""получает сервис по api!"""
+
 def get_service():
+    """получает сервис по api!"""
     load_dotenv(find_dotenv())
     YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
     service = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
@@ -16,14 +18,57 @@ def get_service():
 
 class Channel:
     """инициализация по id"""
+
     def __init__(self, ch_id):
         self.ch_id = ch_id
-    """выводит инфу о канале"""
+        self.__ch_info = get_service().channels().list(id=ch_id, part='snippet,statistics').execute()
+        self.__title = self.title
+
+    def to_json(self, filename):
+        """Отправляет информацию в джисон"""
+        channel_id = self.ch_id
+        data = get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False)
+
+    @property
+    def title(self) -> str:
+        """Геттер title"""
+        channel_title = self.__ch_info.get('items')[0].get('snippet').get('title')
+        return channel_title
+
+    @property
+    def description(self):
+        """Геттер description"""
+        channel_description = self.__ch_info.get('items')[0].get('snippet').get('description')
+        return channel_description
+
+    @property
+    def link(self):
+        """Геттер link"""
+        channel_link = self.__ch_info.get('items')[0].get('snippet').get('customUrl')
+        return channel_link
+
+    @property
+    def subs(self) -> int:
+        """Геттер subs"""
+        channel_subs = self.__ch_info.get('items')[0].get('statistics').get('viewCount')
+        return channel_subs
+
+    @property
+    def videos(self) -> int:
+        """Геттер videos"""
+        channel_videos = self.__ch_info.get('items')[0].get('statistics').get('videoCount')
+        return channel_videos
+
+    @property
+    def views(self):
+        """Геттер views"""
+        channel_views = self.__ch_info.get('items')[0].get('statistics').get('viewCount')
+        return channel_views
+
     def print_info(self):
+        """выводит инфу о канале"""
         channel_id = self.ch_id
         channel = get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         print(channel)
-
-
-vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-vdud.print_info()
