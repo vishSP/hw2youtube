73ce1a4094590120afd33931007c75a149e24c8e
vishSP <hihi64741@gmail.com>
2023-02-28 21:03:51 +0200

    нааписал код и тесты

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
 from utils import Channel
+from utils import Video
+from utils import PLVideo
 from utils import get_service
 import pytest
 import os
@@ -10,6 +12,15 @@ from dotenv import load_dotenv, find_dotenv
 @pytest.fixture
 def channel():
     return Channel(ch_id='UCMCgOm8GZkHp8zJ6l7_hIuA')
+
+@pytest.fixture
+def video():
+    return Video(id='9lO06Zxhu88')
+
+@pytest.fixture
+def plv_video():
+    return PLVideo(id='BBotskuyw_M', id_playlist ='PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+
 
 
 def test_str(channel):
@@ -45,3 +56,9 @@ def test_videos(channel):
 def test_views(channel):
     """тестирует геттер views"""
     assert channel.views == '1947591121'
+
+def test_str_video(video):
+    assert video.__str__() == 'Как устроена IT-столица мира / Russian Silicon Valley (English subs)'
+
+def test_str_plv(plv_video):
+    assert plv_video.__str__() == 'Пушкин: наше все? (Литература)'
